1774d9402e12af0f4b2011c11cfffa92558085b3
Hidekazu Ishikawa <ishikawa_hidekazu@nissin.co.jp>
2019-10-25 14:22:33 +0900

    Updated.

--- discordbot.py
+++ discordbot.py
@@ -11,23 +11,20 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 @client.event
 async def on_message(ctx):
-    # msg_list = ['こんばんは', 'こんにちは', 'おはよー']
     if ctx.author.bot:
         return
     if ctx.content == '!chum':
         with open('chum.txt') as d:
             l_dic = [s.strip() for s in d.readlines()]
         await ctx.send(random.choice(l_dic))
-    if ctx.content == '!sou':
-        with open('sou.txt') as d:
-            l_dic = [s.strip() for s in d.readlines()]
-        await ctx.send(random.choice(l_dic))
-    if ctx.content == '!ken':
-        with open('ken.txt') as d:
-            l_dic = [s.strip() for s in d.readlines()]
-        await ctx.send(random.choice(l_dic))
-    # if ctx.content in msg_list:
-    #     await ctx.send('へいよーぐっつすっす')
+    # if ctx.content == '!sou':
+    #     with open('sou.txt') as d:
+    #         l_dic = [s.strip() for s in d.readlines()]
+    #     await ctx.send(random.choice(l_dic))
+    # if ctx.content == '!ken':
+    #     with open('ken.txt') as d:
+    #         l_dic = [s.strip() for s in d.readlines()]
+    #     await ctx.send(random.choice(l_dic))
 
 
 client.run(token)
