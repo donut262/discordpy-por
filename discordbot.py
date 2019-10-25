79ce793289497e79a3c5af9390c1250a911d7b96
Hidekazu Ishikawa <ishikawa_hidekazu@nissin.co.jp>
2019-10-25 14:07:01 +0900

    Updated.

--- discordbot.py
+++ discordbot.py
@@ -4,49 +4,52 @@ import os
 import random
 import traceback
 
-bot = commands.Bot(command_prefix='d!')
+# bot = commands.Bot(command_prefix='d!')
+client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
-
-@bot.event
-async def on_command_error(ctx, error):
-    await ctx.send(str(error))
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
-@bot.command()
-async def chum(ctx):
-    with open('chum.txt') as d:
-        l_dic = [s.strip() for s in d.readlines()]
-    await ctx.send(random.choice(l_dic))
-
-
-@bot.command()
-async def sou(ctx):
-    with open('sou.txt') as d:
-        l_dic = [s.strip() for s in d.readlines()]
-    await ctx.send(random.choice(l_dic))
-
-
-@bot.command()
-async def ken(ctx):
-    with open('ken.txt') as d:
-        l_dic = [s.strip() for s in d.readlines()]
-    await ctx.send(random.choice(l_dic))
-
-
-@bot.event()
+@client.event
 async def on_message(ctx):
-    msg_list = ['こんばんは', 'こんにちは', 'おはよー']
     if ctx.author.bot:
         return
-    if ctx.content in msg_list:
-        await ctx.send('へいよーぐっつすっす')
+    if ctx.content == '/chum':
+        await ctx.channel.send('ストロベリー')
 
+client.run(token)
 
-
-bot.run(token)
+# @bot.event
+# async def on_command_error(ctx, error):
+#     await ctx.send(str(error))
+#
+#
+# @bot.command()
+# async def chum(ctx):
+#     with open('chum.txt') as d:
+#         l_dic = [s.strip() for s in d.readlines()]
+#     await ctx.send(random.choice(l_dic))
+#
+#
+# @bot.command()
+# async def sou(ctx):
+#     with open('sou.txt') as d:
+#         l_dic = [s.strip() for s in d.readlines()]
+#     await ctx.send(random.choice(l_dic))
+#
+#
+# @bot.command()
+# async def ken(ctx):
+#     with open('ken.txt') as d:
+#         l_dic = [s.strip() for s in d.readlines()]
+#     await ctx.send(random.choice(l_dic))
+#
+#
+# @bot.event()
+# async def on_message(ctx):
+#     msg_list = ['こんばんは', 'こんにちは', 'おはよー']
+#     if ctx.author.bot:
+#         return
+#     if ctx.content in msg_list:
+#         await ctx.send('へいよーぐっつすっす')
+#
+#
+# bot.run(token)
