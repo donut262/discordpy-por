c4fb82371659ce228efdd943d415575c7e810342
Hidekazu Ishikawa <ishikawa_hidekazu@nissin.co.jp>
2019-10-25 14:31:32 +0900

    Updated.

--- discordbot.py
+++ discordbot.py
@@ -11,20 +11,23 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 @client.event
 async def on_message(ctx):
+    msg_list = ['こんばんは', 'こんにちは', 'おはよー']
     if ctx.author.bot:
         return
     if ctx.content == '!chum':
         with open('chum.txt') as d:
             l_dic = [s.strip() for s in d.readlines()]
-        await ctx.send(random.choice(l_dic))
-    # if ctx.content == '!sou':
-    #     with open('sou.txt') as d:
-    #         l_dic = [s.strip() for s in d.readlines()]
-    #     await ctx.send(random.choice(l_dic))
-    # if ctx.content == '!ken':
-    #     with open('ken.txt') as d:
-    #         l_dic = [s.strip() for s in d.readlines()]
-    #     await ctx.send(random.choice(l_dic))
+        await ctx.channel.send(random.choice(l_dic))
+    if ctx.content == '!sou':
+        with open('sou.txt') as d:
+            l_dic = [s.strip() for s in d.readlines()]
+        await ctx.channel.send(random.choice(l_dic))
+    if ctx.content == '!ken':
+        with open('ken.txt') as d:
+            l_dic = [s.strip() for s in d.readlines()]
+        await ctx.channel.send(random.choice(l_dic))
+    if ctx.content in msg_list:
+        await ctx.channel.send('へいよーぐっつすっす')
 
 
 client.run(token)
