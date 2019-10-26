40568e152d564e82c708151faf1fab25951f60ff
Fess <donut262@gmail.com>
2019-10-26 12:03:08 +0900

    Update.

--- discordbot.py
+++ discordbot.py
@@ -8,26 +8,42 @@ import traceback
 client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
+with open('greeting.txt') as f:
+    greeting_list = [s.strip() for s in f.readlines()]
+
+
+@client.event
+async def on_ready():
+    print('ログインしました')
+    print(client.user.name)
+    print(client.user.id)
+    print(discord.__version__)
+    print('------')
+
 
 @client.event
 async def on_message(ctx):
-    msg_list = ['こんばんは', 'こんにちは', 'おはよー', 'ばんちゃー', 'こんにちはー', 'こんばんはー', 'ばんわー', 'おはようございます']
     if ctx.author.bot:
         return
+
     if ctx.content == '!chum':
         with open('chum.txt') as d:
             l_dic = [s.strip() for s in d.readlines()]
         await ctx.channel.send(random.choice(l_dic))
+
     if ctx.content == '!sou':
         with open('sou.txt') as d:
             l_dic = [s.strip() for s in d.readlines()]
         await ctx.channel.send(random.choice(l_dic))
+
     if ctx.content == '!ken':
         with open('ken.txt') as d:
             l_dic = [s.strip() for s in d.readlines()]
         await ctx.channel.send(random.choice(l_dic))
-    if ctx.content in msg_list:
-        await ctx.channel.send('へいよーぐっつすっす')
+
+    for row in greeting_list:
+        if str(ctx.content).startswith(row):
+            await ctx.channel.send("へいよーぐっつすっす\n{message.author.mention}いらっしゃ～い")
 
 
 client.run(token)
