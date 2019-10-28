dda8fc729326eeb7822305a985923fe7fff65bff
Hidekazu Ishikawa <ishikawa_hidekazu@nissin.co.jp>
2019-10-28 11:25:38 +0900

    Updated.

--- discordbot.py
+++ discordbot.py
@@ -3,9 +3,8 @@ from discord.ext import commands
 import os
 import random
 import traceback
-# import get_moe_news
+import get_moe_news
 
-# bot = commands.Bot(command_prefix='d!')
 client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
@@ -35,15 +34,15 @@ async def on_message(ctx):
     #         news_text = news_text + d.get('url') + "\n"
     #     news_text = news_text + "'''"
     #     await ctx.channel.send(news_text)
-    #
-    # if ctx.content == '!moe_news':
-    #     news = get_moe_news.get_moe_news(get_all=False)
-    #     news_text = "'''\n"
-    #     for d in news:
-    #         news_text = news_text + d.get('title') + "\n"
-    #         news_text = news_text + d.get('url') + "\n"
-    #     news_text = news_text + "'''"
-    #     await ctx.channel.send(news_text)
+
+    if ctx.content == '!moe_news':
+        news = get_moe_news.get_moe_news(get_all=False)
+        news_text = "'''\n"
+        for d in news:
+            news_text = news_text + d.get('title') + "\n"
+            news_text = news_text + d.get('url') + "\n"
+        news_text = news_text + "'''"
+        await ctx.channel.send(news_text)
 
     if ctx.content == '!chum':
         with open('chum.txt') as d:
