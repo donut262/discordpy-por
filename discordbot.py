a49d040f3ba2fc7045aaa6432bd30bfd8c345751
Hidekazu Ishikawa <ishikawa_hidekazu@nissin.co.jp>
2019-12-04 15:26:19 +0900

    Updated.

--- discordbot.py
+++ discordbot.py
@@ -43,16 +43,19 @@ async def on_message(ctx):
 
     if ctx.content.startswith('!dice'):
         cmd = ctx.content.split(' ')
+
         if ctx[0:1]:
             rand_min = cmd[1]
         else:
             rand_min = 0
+
         if ctx[1:2]:
             rand_max = cmd[2]
         else:
             rand_max = 1000
+
         dice = random.randint(rand_min, rand_max)
-        await ctx.channel.send(dice)
+        await ctx.channel.send(ctx.author.mention + 'はマジックダイス' + '(' + rand_min + '-' + rand_max +')' + 'を振った！ ' + dice)
 
     if ctx.content == '!chum':
         with open('chum.txt') as d:
